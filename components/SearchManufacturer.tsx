57821094616e0efdcdd37f3c6aedac2649c86dd7
parshva555 <parshvadani@gmail.com>
2023-12-27 17:37:14 +0530

    Adding Search Manufacturer Functionality

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -25,7 +25,19 @@ const SearchManufacturer = ({manufacturer,setManufacturer}:SearchManufacturerPro
                  leaveFrom='opacity-100'
                  leaveTo='opacity-0' afterLeave={() => setQuery('')}>
                     <Combobox.Options>
-
+                        {filteredManufacturers.length === 0 && query!== "" ? (
+                            <Combobox.Option
+                            value={query} className="search-manufacturer__option"
+                            >
+                                Create "{query}"
+                            </Combobox.Option>
+                        ):(
+                            filteredManufacturers.map((item)=> (
+                                <Combobox.Option key={item} className={({active}) => `relative search-manufactrurer__option ${active? 'bg-primary-blue text-white' : 'text-gray-900'}`} value={item}>
+                                    {item}
+                                </Combobox.Option>
+                            ))
+                        )}
                     </Combobox.Options>
                  </Transition>
             </div>
