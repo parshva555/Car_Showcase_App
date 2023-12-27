12ba9f766eb0a4419432965ee5821337eb42cca4
parshva555 <parshvadani@gmail.com>
2023-12-27 18:18:29 +0530

    Browsing through all the cars using combobox docs

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -25,18 +25,30 @@ const SearchManufacturer = ({manufacturer,setManufacturer}:SearchManufacturerPro
                  leaveFrom='opacity-100'
                  leaveTo='opacity-0' afterLeave={() => setQuery('')}>
                     <Combobox.Options>
-                        {filteredManufacturers.length === 0 && query!== "" ? (
-                            <Combobox.Option
-                            value={query} className="search-manufacturer__option"
-                            >
-                                Create "{query}"
-                            </Combobox.Option>
-                        ):(
+                        {
                             filteredManufacturers.map((item)=> (
                                 <Combobox.Option key={item} className={({active}) => `relative search-manufactrurer__option ${active? 'bg-primary-blue text-white' : 'text-gray-900'}`} value={item}>
-                                    {item}
+                                    {({selected, active}) => (
+                                        <>
+                                        <span
+                          className={`block truncate ${
+                            selected ? 'font-medium' : 'font-normal'
+                          }`}
+                        >
+                          {item}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active ? 'text-white' : 'text-teal-600'
+                            }`}
+                          >
+                          </span>
+                        ) : null}
+                                        </>
+                                    )}
                                 </Combobox.Option>
-                            ))
+                            )
                         )}
                     </Combobox.Options>
                  </Transition>
