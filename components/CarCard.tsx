3f6d25745ea2d94d80c492a67989d9377e7e1691
parshva555 <parshvadani@gmail.com>
2024-01-02 20:37:49 +0530

    Car Details Component

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -6,13 +6,14 @@ import Image from 'next/image';
 import { CarProps } from '@/types';
 import { CustomButton } from '.';
 import { calculateCarRent } from '@/utils';
+import {CarDetails} from '.';
 interface CarCardProps{
     car:CarProps
 }
 const CarCard = ({car} : CarCardProps) => {
     const {city_mpg, year, make, model, transmission, drive} = car;
     const carRent = calculateCarRent(city_mpg,year);
-    const [open, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
   return (
     <div className='car-card group'>
         <div className='car-card__content'>
@@ -51,8 +52,9 @@ const CarCard = ({car} : CarCardProps) => {
                 <CustomButton title='View More' containerStyles='w-full py-[16px] rounded-full bg-primary-blue' textStyles="text-white text-[14px] leading=[17px] font-=bold" rightIcon="/right-arrow.svg" handleClick={() => setIsOpen(true)}/>
             </div>
         </div>
+        <CarDetails isOpen={isOpen} closeModal={()=> setIsOpen(false)} car={car} />
     </div>
   )
 }
 
-export default CarCard
+export default CarCard;
